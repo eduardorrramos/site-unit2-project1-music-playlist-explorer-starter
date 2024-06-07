a37da00f2710db42c3bfbe3e1fcf09187edf6e65
Eduardo Ramos <eduardoramos@meta.com>
2024-06-06 21:20:57 -0700

    gave each card own id, use id to open specific song modals

--- music-playlist-creator/storage.js
+++ music-playlist-creator/storage.js
@@ -6,8 +6,11 @@ function main(playlistdatajson){
 for (let index= 0; index < playlistdatajson.length; index ++) {
     
     let one = playlistdatajson[index];
-    console.log(one.songs[0])
-    console.log(one.songs)
+    // console.log(one.songs[0])
+    // console.log(one.songs)
+    // console.log(one)
+    let oneid = one.playlistID;
+    // console.log(oneid)
     /*one.songs look at structure presented in data.js, songs for 
     one playlist already, [0] getting speciifc songs
      <div class="card">
@@ -16,39 +19,50 @@ for (let index= 0; index < playlistdatajson.length; index ++) {
                 <h4 class="creator">Creator Name</h4>
             </div>
             */
-
+    // const list = createNewElement
     card_container.innerHTML +=
-    `<div class="card"> 
-        
+    `<div class="card" id="${oneid}">
         <img class="image" src="${one.playlist_art}" alt = "oops">
-        <div class="title">
-                ${one.playlist_name}
-            <p class="creator">${one.playlist_creator}</p>
-            <p>${one.likeCount}</p>
-            <p>${one.playlistID}</p>
-            <p>${one.songs[0]}</p>
-        </div>
+        <div class="title">${one.playlist_name}</div>
+        <p class="creator">${one.playlist_creator}</p>
+        <p>${one.likeCount}</p>
+        <p>${one.playlistID}</p>
         
     </div>
-        `
-    
-
+        `;
+        // <p>${one.songs}</p></div>
+        // playlistdatajson[index].addEventListener('click', () =>{
+            // document.style.display: "block";
+        // });
+        // playlistList.appendChild(listItem);
     // function opensongs(){
         // document.style.display = "block";
     // }
-
 /*for (let index= 0; index < playlists.len(); index ++) {
     playlists[index].addEventListener('click', (event) => {
-
     }
-
 }*/
 }
-// CHANGE THIS INDEX CHANGE THIS INDEX CHANGE THE PARAMETER THE GREATER EQUAL
-const allcards = card_container.getElementsByClassName("card");
-console.log(allcards);
-// allcards.forEach()hudiikvfgtebceckrlkeldnfiirudrtjbgkhvhkkgliugglhkciutkdceihdhhkt
-
+for (let index = 0; index < playlistdatajson.length; index++) {
+    let onecard = playlistdatajson[index];
+    let onecardid = onecard.playlistID;
+    // console.log(onecard);
+    // console.log(onecardid);
+    const cardElement = document.getElementById(index);
+    cardElement.addEventListener('click', () => {
+        open(onecardid);
+    });
+    // let grr = document.getElementById(index).addEventListener('click', open(onecardid));
+    // console.log(grr);
+    // 
 }
-
+function open(filler){
+    console.log(filler)
+    // modal.style.display = "block";
+}
+// CHANGE THIS INDEX CHANGE THIS INDEX CHANGE THE PARAMETER THE GREATER EQUAL
+// const allcards = card_container.getElementsByClassName("card");
+// console.log(allcards);
+// allcards.forEach()hudiikvfgtebceckrlkeldnfiirudrtjbgkhvhkkgliugglhkciutkdceihdhhkt
+}
 main(data.playlists);
