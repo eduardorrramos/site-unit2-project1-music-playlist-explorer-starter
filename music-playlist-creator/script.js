bf68b7d45aa67cfc2b731c97b85d4b990908594e
Eduardo Ramos <eduardoramos@meta.com>
2024-06-07 09:30:36 -0700

    tldr

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,25 +1,72 @@
 var modal = document.getElementById("modal");
+var btn=document.getElementById("button");
+var close=document.getElementById("close");
+var modalclose=document.getElementById("modalclose");
 
-function btnonclick(index, cardid) {
+btn.onclick = function() {
     modal.style.display = "block";
-    let onecard = data.playlists[index];
-    let onecardsongs = onecard.songs;
-    let onesong = onecardsongs[cardid];
-    // let onesong = document.getElementById(cardid);
-    // data.playlists.playlistID[0]
-    modalcontent.innerHTML = `
-    <div class="song">
-    <img src="${onesong.cover_art}" alt="Song cover art">
-    <div class="info">
-    <h3 class="title">${onesong.title}</h3>
-    <h3 class="artist">${onesong.artist}</h3>
-    <p class="album">${onesong.album}</p>
-    </div>
-    </div>
-    </div>`;
-    modal.addEventListener('click', () => {
-        closeonclick();
-     });
+    // close.style.display = "block";
 }
-closeonclick = function() {
-    modal.style.display = "none"; }
+close.onclick = function() {
+    modal.style.display = "none";
+}
+
+
+// function cardonclick(playlistindex) {
+//     modal.style.display="block";
+//     modalcontent.innerHTML = `
+//     <div id="modalcontent">
+//     <header>
+//     <img src="${onesong.cover_art}">
+//     <div class="info">
+//     <h3 class="title">${onesong.title}</h3>
+//     <h3 class="artist">${onesong.artist}</h3>
+//     <p class="album">${onesong.album}</p>
+//     </header>
+//     </div>
+//     </div>`;
+//     modal.addEventListener('click', () => {
+//         closeonclick();
+//      });
+
+
+
+//     let onecard = data.playlists[index];
+//     let onecardsongs = onecard.songs;
+//     let onesong = onecardsongs[cardid];
+//     // let onesong = document.getElementById(cardid);
+    
+// closeonclick = function() {
+//     modal.style.display = "none"; }
+
+    
+//     // <div>
+//     // <img src="${onesong.cover_art}" alt="Song cover art"/>
+//     // <div class="info">
+//     // <h3 class="title">${onesong.title}</h3>
+//     // <h3 class="artist">${onesong.artist}</h3>
+//     // <p class="album">${onesong.album}</p>
+//     // </div>
+//     // </div>`
+// }/*
+// <div id="modal" class="modal">
+//   <div class="modal-content">
+//       <button class="close">&times;</button>
+//       <header>
+//           <img src="assets/img/song.png">
+//           <h3 class="title">Playlist Title </h3>
+//           <h4 class="creator">Creator Name</h4>
+//           <h4 class="creator">Album Name</h4>
+
+//       </header>
+//       <div>
+//           <img src="assets/img/song.png">
+//           <h3 class="title">Playlist Title </h3>
+//           <h4 class="creator">Creator Name</h4>
+//           <h4 class="creator">Album Name</h4>
+
+//       </div>
+//       </div>
+//       </div>
+    
+//         */
--- music-playlist-creator/storage.js
+++ music-playlist-creator/storage.js
@@ -1,32 +1,88 @@
-
 var card_container = document.getElementById("playlist-cards")
-function main(playlistdatajson){
-/*console.log(playlistdatajson);*/
+var modal=document.getElementById("modal");
+var modal_content = document.getElementById("modal_content")
 
-for (let index= 0; index < 8; index ++) {
+function main(allplaylists){
+    for (let index= 0; index < 9; index ++) {
     
-    let one = data.playlists[index];
-    let oneid = one.playlistID;
-
-    card_container.innerHTML +=
+        let one = allplaylists[index];
+        card_container.innerHTML +=
     `<div class="card" id="${oneid}">
         <img class="image" src="${one.playlist_art}" alt = "oops">
         <div class="title">${one.playlist_name}</div>
         <p class="creator">${one.playlist_creator}</p>
-        <div class="likebutton"> <i class-"fas fa-heart"></i></div>
         <p class="likecount">${one.likeCount}</p>
-    </div>
+        </div>
         `;
-}
-for (let index = 0; index < 8; index++) {
-    const cardElement = document.getElementById(index);
+        }
+
+
+    for (let index = 0; index < 9; index++) {
+        const cardElement = document.getElementById(index);
     let cardid = cardElement.id;
     cardElement.addEventListener('click', () => {
        btnonclick(index, cardid);
-    });
+    });}
+    //     let one = allplaylists[index];
+    //     let oneid = one.playlistID;
+    //     const cardElement = document.getElementById(index);
+    // cardElement.addEventListener('click', () => { open(one);
+    // });
+    // function open(onecard){
+    //     console.log(filler)
+    // }
+    // for (let index= 0; index < 9; index ++) {
+    //     let onecard = allplaylists[index];
+    //     let onecardsongs = onecard.songs;
+    //     for (let i = 0; i < onecardsongs.length; i++) {
+    //         let onesong = onecardsongs[i];
+    
+    //     modal_content.innerHTML +=
+    //     `<div class="song">
+    //     <img src="${onesong.cover_art}" alt="Song cover art">
+    //     <div class="info">
+    //         <h3 class="title">${onesong.title}</h3>
+    //         <p class="artist">${onesong.artist}</p>
+    //         <p class="album">${onesong.album}</p>
+    //     </div>
+    // </div>`;
+    //     }
+    // }
 }
 
+// var card_container = document.getElementById("playlist-cards");
+// function main(allplaylists){
 
-    }
+//     let oneplaylist = data.playlists[0];
+//     let oneplaylistid = oneplaylist.playlistID;
+// for (let index= 0; index < 9; index ++) {
+//     oneplaylist = data.playlists[index];
+//     oneplaylistid = oneplaylist.playlistID;
 
-main(data.playlists);
+//     card_container.innerHTML +=
+//     `<div class="card" id="${oneplaylistid}">
+//         <img class="image" src="${oneplaylist.playlist_art}" alt = "oops">
+//         <div class="title">${oneplaylist.playlist_name}</div>
+//         <p class="creator">${oneplaylist.playlist_creator}</p>
+//         <div hjirvnccivuijggjtrgdkktbtbnfievtass="like-button" id="${oneplaylistid}">
+//         <span>&#x2764;&#xFE0F;</span>
+//         </div>
+//         <p class="likecount">${oneplaylist.likeCount}</p>
+//         </div>
+//         `;
+//         const cardElement = document.getElementById(oneplaylistid);
+//     cardElement.addEventListener('click', () => {
+//       btnonclick(index, oneplaylistid);
+    // oneplaylist.addEventListener('click', () => {
+    // btnonclick(index, cardid);
+    
+
+// for (let index = 0; index < 9; index++) {
+//     let cardElement = document.getElementById(index);
+//     let cardid = cardElement.id;
+//     cardElement.addEventListener('click', () => {
+//        btnonclick(index, cardid);
+//     //    btnonclick(index, data.playlists[index].playlistID);
+//     });
+// }
+
