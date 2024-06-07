cca62a68a6c712811f7f594c728ada3b4a0a96c2
Eduardo Ramos <eduardoramos@meta.com>
2024-06-07 04:46:52 -0700

    some fucntionality

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,11 +1,4 @@
-var btn=document.getElementById("button");
-var close=document.getElementById("close");
-// class name verusus id, specifies array as opposed to a single element
 var modal = document.getElementById("modal");
-var modalclose=document.getElementById("modalclose");
-
-// var modal=document.getElementById("modal");
-
 
 function btnonclick(index, cardid) {
     modal.style.display = "block";
@@ -22,24 +15,11 @@ function btnonclick(index, cardid) {
     <h3 class="artist">${onesong.artist}</h3>
     <p class="album">${onesong.album}</p>
     </div>
+    </div>
     </div>`;
+    modal.addEventListener('click', () => {
+        closeonclick();
+     });
 }
-close.onclick = function() {
-    console.log("closing")
-    modal.style.display = "none";
-   
-}
-
-//     let onecard = data.playlists[index];
-//     let onecardsongs = onecard.songs;
-//     let onesong = onecardsongs[cardid];
-
-//     modal_content.innerHTML +=
-//     `<div class="song">
-//     <img src="${onesong.cover_art}" alt="Song cover art">
-//     <div class="info">
-//         <h3 class="title">${onesong.title}</h3>
-//         <p class="artist">${onesong.artist}</p>
-//         <p class="album">${onesong.album}</p>
-//     </div>
-// </div>`;
+closeonclick = function() {
+    modal.style.display = "none"; }
--- music-playlist-creator/storage.js
+++ music-playlist-creator/storage.js
@@ -1,11 +1,11 @@
+
 var card_container = document.getElementById("playlist-cards")
-var modalcontent = document.getElementById("modalcontent")
 function main(playlistdatajson){
 /*console.log(playlistdatajson);*/
 
-for (let index= 0; index < playlistdatajson.length; index ++) {
+for (let index= 0; index < 8; index ++) {
     
-    let one = playlistdatajson[index];
+    let one = data.playlists[index];
     let oneid = one.playlistID;
 
     card_container.innerHTML +=
@@ -13,60 +13,20 @@ for (let index= 0; index < playlistdatajson.length; index ++) {
         <img class="image" src="${one.playlist_art}" alt = "oops">
         <div class="title">${one.playlist_name}</div>
         <p class="creator">${one.playlist_creator}</p>
+        <div class="likebutton"> <i class-"fas fa-heart"></i></div>
         <p class="likecount">${one.likeCount}</p>
     </div>
         `;
-
-        // playlistdatajson[index].addEventListener('click', () =>{
-            // document.style.display: "block";
-        // });
-        // playlistList.appendChild(listItem);
-    // function opensongs(){
 }
-for (let index = 0; index < playlistdatajson.length; index++) {
-    
-//     let one = playlistdatajson[index];
-//     let oneid = one.playlistID;
+for (let index = 0; index < 8; index++) {
     const cardElement = document.getElementById(index);
     let cardid = cardElement.id;
     cardElement.addEventListener('click', () => {
        btnonclick(index, cardid);
     });
-    // let grr = document.getElementById(index).addEventListener('click', open(onecardid));
-    // console.log(grr);
-//     // 
-// }
 }
 
 
-// var modalcontent = document.getElementById("modalcontent")
-
-    // modalcontent.innerHTML += `
-    // <div class="song">
-    // <img src="${onesong.cover_art}" alt="Song cover art">
-    // <div class="info">
-    // <h3 class="title">${onesong.title}</h3>
-    // <p class="artist">${onesong.artist}</p>
-    // <p class="album">${onesong.album}</p>
-    // </div>
-    // </div>`;
-    
-    // function btnonclick() {
-    //     modal.style.display = "block";
-    //     console.log("opening")
-    //     // close.style.display = "block";
-    // }
-    // modal.style.display = "block";
-    // modal.style.display = "block";
-
-// CHANGE THIS INDEX CHANGE THIS INDEX CHANGE THE PARAMETER THE GREATER EQUAL
-// const allcards = card_container.getElementsByClassName("card");
-// console.log(allcards);
-// allcards.forEach()hudiikvfgtebceckrlkeldnfiirudrtjbgkhvhkkgliugglhkciutkdceihdhhkt
-
-
-
-    
     }
 
 main(data.playlists);
