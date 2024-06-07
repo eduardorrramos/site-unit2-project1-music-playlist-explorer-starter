c59908ca6082c4ab25596b4bb78f689f9a0bf7b4
Eduardo Ramos <eduardoramos@meta.com>
2024-06-07 02:24:10 -0700

    fae

--- music-playlist-creator/storage.js
+++ music-playlist-creator/storage.js
@@ -26,11 +26,12 @@ for (let index= 0; index < playlistdatajson.length; index ++) {
         <img class="image" src="${one.playlist_art}" alt = "oops">
         <div class="title">${one.playlist_name}</div>
         <p class="creator">${one.playlist_creator}</p>
-        <p>${one.likeCount}</p>
-        <p>${one.playlistID}</p>
-        
+        <p class="likecount">${one.likeCount}</p>
+        <button onclick="open(id)">View Playlist</button>
     </div>
         `;
+        //         <p class="playlisti${one.playlistID}</p>
+
         // <p>${one.songs}</p></div>
         // playlistdatajson[index].addEventListener('click', () =>{
             // document.style.display: "block";
@@ -44,72 +45,43 @@ for (let index= 0; index < playlistdatajson.length; index ++) {
     }
 }*/
 }
-for (let index = 0; index < playlistdatajson.length; index++) {
+
+// for (let index = 0; index < playlistdatajson.length; index++) {
     
-    let one = playlistdatajson[index];
-    let oneid = one.playlistID;
-    // console.log(onecard);
-    // console.log(onecardid);
-    const cardElement = document.getElementById(oneid);
-    cardElement.addEventListener('click', () => {
-        open(one);
-    });
-    // let grr = document.getElementById(index).addEventListener('click', open(onecardid));
-    // console.log(grr);
-    // 
-}
-function open(onecard){
-    console.log(filler)
+//     let one = playlistdatajson[index];
+//     let oneid = one.playlistID;
+//     // console.log(onecard);
+//     // console.log(onecardid);
+//     const cardElement = document.getElementById(oneid);
+//     cardElement.addEventListener('click', () => {
+//         open(one);
+//     });
+//     // let grr = document.getElementById(index).addEventListener('click', open(onecardid));
+//     // console.log(grr);
+//     // 
+// }
+function open(id){
+    let onesong= getElementById(id);
+    modal_content.innerHTML += `
+    <div class="song">
+    <img src="${onesong.cover_art}" alt="Song cover art">
+    <div class="info">
+    <h3 class="title">${onesong.title}</h3>
+    <p class="artist">${onesong.artist}</p>
+    <p class="album">${onesong.album}</p>
+    </div>
+    </div>`;
+    modal.style.display = "block";
     // modal.style.display = "block";
 }
 // CHANGE THIS INDEX CHANGE THIS INDEX CHANGE THE PARAMETER THE GREATER EQUAL
 // const allcards = card_container.getElementsByClassName("card");
 // console.log(allcards);
 // allcards.forEach()hudiikvfgtebceckrlkeldnfiirudrtjbgkhvhkkgliugglhkciutkdceihdhhkt
-for (let index= 0; index < playlistdatajson.length; index ++) {
-    let onecard = playlistdatajson[index];
-    let onecardsongs = onecard.songs;
-    console.log(onecardsongs)
-    console.log(onecardsongs[0])
 
 
-    /*one.songs look at structure presented in data.js, songs for 
-    one playlist already, [0] getting speciifc songs
-     <div class="card">
-                <img class="image" src="assets/img/playlist.png" alt="oops">
-                <h3 class="title">Playlist Title </h3>
-                <h4 class="creator">Creator Name</h4>
-            </div>
-            */
-    // const list = createNewElement
-    for (let i = 0; i < onecardsongs.length; i++) {
-        let onesong = onecardsongs[i];
 
-    modal_content.innerHTML +=
-    `<div class="song">
-    <img src="${onesong.cover_art}" alt="Song cover art">
-    <div class="info">
-        <h3 class="title">${onesong.title}</h3>
-        <p class="artist">${onesong.artist}</p>
-        <p class="album">${onesong.album}</p>
-    </div>
-</div>`;
-
-    // <header>
-    //     <img src="assets/img/song.png">
-    //     <h3 class="title">Playlist Title </h3>
-    //     <h4 class="creator">Creator Name</h4>
-    //     <h4 class="creator">Album Name</h4>
     
-    // </header>
-    // <div>
-    //     <img src="assets/img/song.png">
-    //     <h3 class="title">Playlist Title </h3>
-    //     <h4 class="creator">Creator Name</h4>
-    //     <h4 class="creator">Album Name</h4>
-    // </div></div>
-    // ';
-            }
-}
-}
+    }
+
 main(data.playlists);
