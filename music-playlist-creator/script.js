636f15e37fc6369c9135d5d2ab654721da953344
Eduardo Ramos <eduardoramos@meta.com>
2024-06-06 22:59:43 -0700

    ok i have playlists and songs imported and it opens and closes now i need to format

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,10 +1,9 @@
 var modal=document.getElementById("modal");
 var btn=document.getElementById("button");
-var close=document.getElementsByClassName("close");
+var close=document.getElementById("close");
 // class name verusus id, specifies array as opposed to a single element
 
 var modalclose=document.getElementById("modalclose");
-
 
 btn.onclick = function() {
     modal.style.display = "block";
@@ -12,7 +11,7 @@ btn.onclick = function() {
     // close.style.display = "block";
 }
 
-close[0].onclick = function() {
+close.onclick = function() {
     console.log("closing")
     modal.style.display = "none";
     // how none affects event listeners, if not j pivot into 
--- music-playlist-creator/storage.js
+++ music-playlist-creator/storage.js
@@ -1,4 +1,5 @@
 var card_container = document.getElementById("playlist-cards")
+var modal_content = document.getElementById("modal_content")
 
 function main(playlistdatajson){
 /*console.log(playlistdatajson);*/
@@ -44,19 +45,20 @@ for (let index= 0; index < playlistdatajson.length; index ++) {
 }*/
 }
 for (let index = 0; index < playlistdatajson.length; index++) {
-    let onecard = playlistdatajson[index];
-    let onecardid = onecard.playlistID;
+    
+    let one = playlistdatajson[index];
+    let oneid = one.playlistID;
     // console.log(onecard);
     // console.log(onecardid);
-    const cardElement = document.getElementById(index);
+    const cardElement = document.getElementById(oneid);
     cardElement.addEventListener('click', () => {
-        open(onecardid);
+        open(one);
     });
     // let grr = document.getElementById(index).addEventListener('click', open(onecardid));
     // console.log(grr);
     // 
 }
-function open(filler){
+function open(onecard){
     console.log(filler)
     // modal.style.display = "block";
 }
@@ -64,5 +66,50 @@ function open(filler){
 // const allcards = card_container.getElementsByClassName("card");
 // console.log(allcards);
 // allcards.forEach()hudiikvfgtebceckrlkeldnfiirudrtjbgkhvhkkgliugglhkciutkdceihdhhkt
+for (let index= 0; index < playlistdatajson.length; index ++) {
+    let onecard = playlistdatajson[index];
+    let onecardsongs = onecard.songs;
+    console.log(onecardsongs)
+    console.log(onecardsongs[0])
+
+
+    /*one.songs look at structure presented in data.js, songs for 
+    one playlist already, [0] getting speciifc songs
+     <div class="card">
+                <img class="image" src="assets/img/playlist.png" alt="oops">
+                <h3 class="title">Playlist Title </h3>
+                <h4 class="creator">Creator Name</h4>
+            </div>
+            */
+    // const list = createNewElement
+    for (let i = 0; i < onecardsongs.length; i++) {
+        let onesong = onecardsongs[i];
+
+    modal_content.innerHTML +=
+    `<div class="song">
+    <img src="${onesong.cover_art}" alt="Song cover art">
+    <div class="info">
+        <h3 class="title">${onesong.title}</h3>
+        <p class="artist">${onesong.artist}</p>
+        <p class="album">${onesong.album}</p>
+    </div>
+</div>`;
+
+    // <header>
+    //     <img src="assets/img/song.png">
+    //     <h3 class="title">Playlist Title </h3>
+    //     <h4 class="creator">Creator Name</h4>
+    //     <h4 class="creator">Album Name</h4>
+    
+    // </header>
+    // <div>
+    //     <img src="assets/img/song.png">
+    //     <h3 class="title">Playlist Title </h3>
+    //     <h4 class="creator">Creator Name</h4>
+    //     <h4 class="creator">Album Name</h4>
+    // </div></div>
+    // ';
+            }
+}
 }
 main(data.playlists);
