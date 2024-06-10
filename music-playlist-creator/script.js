bfb434c3de913174be81013828f02b1a1e19669b
Eduardo Ramos <eduardoramos@meta.com>
2024-06-10 05:29:48 -0700

    implementation complete

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,27 +1,59 @@
 var modal=document.getElementById("modal");
-var btn=document.getElementById("button");
+var btn=document.getElementById("playlistbutton");
 var close=document.getElementById("close");
+var shufflebutton = document.getElementById("shufflebutton");
+var abouttoclear = document.getElementsByClassName("modal_content");
 
-
+ 
 // class name verusus id, specifies array as opposed to a single element
-
 // var modalclose=document.getElementById("modalclose");
-
+function populate(song){
+    modal_content.innerHTML +=
+    `<div class="song">
+    <img src="${song.cover_art}" alt="Song cover art" class ="songimage">
+    <div class="songinfo">
+    <h3 class="songtitle">${song.title}</h3>
+    <p class="songartist">${song.artist}</p>
+    <p class="songalbum">${song.album}</p>
+    <p class="songduration">${song.duration}</p>
+    <p class="songid">${song.songID}</p>
+    </div>
+</div>`;
+btn.onclick()
+}
 btn.onclick = function() {
     modal.style.display = "block";
+    // shufflebutton.style.display= "block";
     // close.style.display = "block";
 }
-
 close.onclick = function() {
     modal.style.display = "none";
-    var abouttoclear = document.getElementsByClassName("modal_content");
     abouttoclear[0].innerHTML = "";
+    // manually adding event listener myself, hidden, click event listener
+}
+shufflebutton.onclick = function() {
+    var currentid = parseInt(document.querySelector('.songid').textContent);
+    let playlistid = 0
+    // this is where i classify songid into playlist id
+    if (currentid <= 44) { playlistid = 8} if (currentid <= 39) {playlistid = 7}if (currentid <= 34) {playlistid = 6}if (currentid <= 29) {playlistid = 5}if (currentid <= 24) 
+    {playlistid = 4}if (currentid <= 19) {playlistid = 3}if (currentid <= 14) {playlistid = 2}if (currentid <= 9) {playlistid = 1}if (currentid <= 4) {playlistid = 0}
+
+    let arrayofsongs = data.playlists[playlistid].songs
+    let oneplaylist = data.playlists[playlistid]
+
+    for (let i = arrayofsongs.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [arrayofsongs[i], arrayofsongs[j]] = [arrayofsongs[j], arrayofsongs[i]];
+    }
+        abouttoclear[0].innerHTML = "";
+        modal_content.innerHTML +=
+    `<div class="playlist">
+    <img src="${oneplaylist.playlist_art}" alt="Song cover art" class="playlistimage">
+    <div class="playlistinfo">
+    <h3 class="songtitle">${oneplaylist.playlist_name}</h3>
+    <p class="songartist">${oneplaylist.playlist_creator}</p></div>
+    </div>`;
+    arrayofsongs.forEach(populate);        
     // how none affects event listeners, if not j pivot into 
     // manually adding event listener myself, hidden, click event listener
 }
-// function closeonclick() {
-//     // console.log("closing")
-//     modal.style.display = "none";
-//     // how none affects event listeners, if not j pivot into 
-//     // manually adding event listener myself, hidden, click event listener
-// }
--- music-playlist-creator/storage.js
+++ music-playlist-creator/storage.js
@@ -1,91 +1,112 @@
 var card_container = document.getElementById("playlist-cards")
 var modal_content = document.getElementById("modal_content")
+var abouttoclear = document.getElementsByClassName("modal_content");
+
+/*takes care of liking songs through button and changing innerHTML*/
+function increment(index) {
+    data.playlists[index].likeCount += 1;
+    let one = data.playlists[index];
+    let oneplaylistid = one.playlistID;
+    const card = document.getElementById(oneplaylistid);
+    card.querySelector('.likecount').innerHTML = data.playlists[index].likeCount;
+}
+/*takes playlist and songs, shuffles them, clears innerhtml and repopulates*/
+function shuffle(id){
+    let oneplaylist= data.playlists[id];
+    let arrayofsongs= oneplaylist.songs;
+    for (let i = arrayofsongs.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [arrayofsongs[i], arrayofsongs[j]] = [arrayofsongs[j], arrayofsongs[i]];
+    }
+        abouttoclear[0].innerHTML = "";
+        arrayofsongs.forEach(populate);
+}
 
 function main(playlistdatajson){
-/*console.log(playlistdatajson);*/
 
+/*populates initial screen with all of our playlists */
 for (let index= 0; index < playlistdatajson.length; index ++) {
-    
     let one = playlistdatajson[index];
-    let oneid = one.playlistID;
+    let oneplaylistid = one.playlistID;
     card_container.innerHTML +=
-    
-    `<div class="card" id=${oneid}>
+    `<div class="card" id=${oneplaylistid}>
         <img class="image" src="${one.playlist_art}" alt = "oops">
         <div class="title">${one.playlist_name}</div>
         <p class="creator">${one.playlist_creator}</p>
+        <div class="together">
         <p class="likecount">${one.likeCount}</p>
-        <button id=${oneid} onclick="increment${oneid}" class="heart">&#x2764;&#xFE0F;</button>
-    </div>
+        <p class="heart" id=${oneplaylistid} >💿</p> </div>
+        </div>
         `;
 }
-/*go thru array of heart buttons*/
-const hearts = document.getElementsByClassName("heart");
-for (let i =0 ; i < hearts.length; i++) {
-    hearts[i].onclick = function() {
-    increment(this); }
-    }
-// }
-function increment(heart) {
-    index = heart.id;
-    data.playlists[index].likeCount += 1;
-    const likeCountElement = document.getElementById(`${index}-likecount`);
-    likeCountElement.innerText = data.playlists[index].likeCount;
-}
-// increment()
-    //event.target.parentNode.likeCount += 1;
-    /*bc this extracts ^ id from parent so get playlist id*/
-    /* if false, want to update liekcount*/
-    // event.target.parentNode.likeCount += 1; no need to update bc cyrrently off
-    // console.log(event.target.parentNode.id)
-
-
-
+/* adds event listener to each main screen playlist, either modal opens or like count increments if card or like button are pressed*/
 for (let index = 0; index < playlistdatajson.length; index++) {
-    
     let one = playlistdatajson[index];
-    let oneid = one.playlistID;
-    // console.log(onecard);
-    // console.log(onecardid);
-    const cardElement = document.getElementById(oneid);
-    cardElement.addEventListener('click', () => {
-        openplaylist(oneid);
+    let oneplaylistid = one.playlistID;
+    const cardcontainer = document.getElementById(oneplaylistid);
+    cardcontainer.addEventListener('click', function(event) {
+        if (event.target.classList.contains('heart')) {
+            increment(oneplaylistid);
+        }
+       else {
+        openplaylist(oneplaylistid);
+       }
     });
-    
-    // let grr = document.getElementById(index).addEventListener('click', open(onecardid));
-    // console.log(grr);
-    // 
 }
-// function open(onecard){
-//     // console.log(filler)
-//     // modal.style.display = "block";
-// }
-// CHANGE THIS INDEX CHANGE THIS INDEX CHANGE THE PARAMETER THE GREATER EQUAL
-// const allcards = card_container.getElementsByClassName("card");
-// console.log(allcards);
-// allcards.forEach()hudiikvfgtebceckrlkeldnfiirudrtjbgkhvhkkgliugglhkciutkdceihdhhkt
+/*actually populates playlist songs into modal and opens it too*/
 function populate(song){
     modal_content.innerHTML +=
     `<div class="song">
-    <img src="${song.cover_art}" alt="Song cover art">
-    
-        <h3 class="title">${song.title}</h3>
-        <p class="artist">${song.artist}</p>
-        <p class="album">${song.album}</p>
-   
+    <img src="${song.cover_art}" alt="Song cover art" class ="songimage">
+    <div class="songinfo">
+    <h3 class="songtitle">${song.title}</h3>
+    <p class="songartist">${song.artist}</p>
+    <p class="songalbum">${song.album}</p>
+    <p class="songduration">${song.duration}</p>
+    <p class="songid">${song.songID}</p>
+    </div>
 </div>`;
 btn.onclick()
-} /*now need to pass id into openplaylist*/
-function openplaylist(givenid) {
-    let oneplaylist=playlistdatajson[givenid];
-    let arrayofsongs=oneplaylist.songs;
-    // console.log(arrayofsongs);
+}
+/* clears modal and inserts playlist info, gets its songs and feeds them into prior populate method */
+function openplaylist(oneplaylistid) {
+    let oneplaylist= playlistdatajson[oneplaylistid];
+    let arrayofsongs= oneplaylist.songs;
+    modal_content.innerHTML = ""
+    modal_content.innerHTML +=
+    `<div class="playlist">
+    <img src="${oneplaylist.playlist_art}" alt="Song cover art" class="playlistimage">
+    <div class="playlistinfo">
+    <h3 class="songtitle">${oneplaylist.playlist_name}</h3>
+    <p class="songartist">${oneplaylist.playlist_creator}</p>
+    </div>
+</div>`;
     arrayofsongs.forEach(populate);
 }
-console.log("right before openplay in main")
-// openplaylist(3)
+// THIS IS FOR THE FEATURED PAGE
+
+function startfeature() {
+var featured = document.getElementById("featuremodal");
+let one = data.playlists[1];
+let oneplaylistid = one.playlistID;
+if (featured) {
+featured.innerHTML +=
+`<div class="ccard" id=${oneplaylistid}>
+    <img class="iimage" src="${one.playlist_art}" alt = "oops">
+    <div class="ttitle">${one.playlist_name}</div>
+    <p class="ccreator">${one.playlist_creator}</p>
+    </div>
+    `;
+}
+
+}
+
+  
+// let featuredplaylist = data.playlists[0];
+// featuremodal.innerHTML = ""
+// featuremodal.innerHTML += 
+// '<img src="${featuredplaylist.playlist_art"}> ';
+
 } // end of main
+
 main(data.playlists);
-// console.log(data.playlists[0].songs);
-// console.log(" outside of main")
-// openplaylist(2)
